7414a7d5f21758f89c0221318a015091d4edce7e
Author: jpdhernandez <jpdhernandez@gmail.com>
2017-01-22 13:51:22 -0500

    Setup project
     - Create express boiler plate
     - initialize and install dependencies

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// This app will take in incoming HTTP requests
+const app = express();
+
+// Watch for incoming requests of method GET
+// to the route http://localhost:3050/api
+app.get('/api', (req, res) => {
+    res.send('Hello, world!');
+});
+
+module.exports = app;
