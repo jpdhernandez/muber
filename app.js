a4e7163f44e9762a2f0942e4fea5abc3c172c4de
jpdhernandez <jpdhernandez@gmail.com>
2017-01-22 17:28:34 -0500

    Refactor and change project structure
     - modularize for controllers and models

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const express = require('express');
-
+const routes = require('./routes/routes');
 // This app will take in incoming HTTP requests
 const app = express();
 
-// Watch for incoming requests of method GET
-// to the route http://localhost:3050/api
-app.get('/api', (req, res) => {
-    res.send({ hi: "there" });
-});
+routes(app);
 
 module.exports = app;
