9cfd5d2ff1eb90bde380a9c230e8d1c850596186
jpdhernandez <jpdhernandez@gmail.com>
2017-01-22 16:23:14 -0500

    Define and test a route handler
     - install mocha, nodemon, supertest
     - add a test folder for test files

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 // Watch for incoming requests of method GET
 // to the route http://localhost:3050/api
 app.get('/api', (req, res) => {
-    res.send('Hello, world!');
+    res.send({ hi: "there" });
 });
 
 module.exports = app;
