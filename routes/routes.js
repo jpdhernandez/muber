9ad8c3958a072e6c995d4b0105b9c500b40aaccb
jpdhernandez <jpdhernandez@gmail.com>
2017-01-22 19:28:54 -0500

    Add driver model
     - create driver route
     - add body parser middleware
     - test driver creation
     - setup mongoose and connect to db

--- routes/routes.js
+++ routes/routes.js
@@ -5,4 +5,6 @@ module.exports = (app) => {
     // Watch for incoming requests of method GET
     // to the route http://localhost:3050/api
     app.get('/api', DriversController.greeting);
+
+    app.post('/api/drivers', DriversController.create);
 };
